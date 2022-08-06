b137ab55728ad8b2ea52f3eb99865c4a3e117b24
Prohibidou <veramaxy@hotmail.com>
2022-08-06 20:52:07 -0300

    cxcxv

---
+++ probandoprobando7/src/probandoprobando7/maxi.java
@@ -0,0 +1,10 @@
+package probandoprobando7;
+
+public class maxi {
+
+	public static void main(String[] args) {
+int a =5;
+		System.out.println(adssad);
+	}
+uiooiuouiooiu
+}
---
+++ probandoprobando7/src/probandoprobando7/trtytyr.java
@@ -0,0 +1,12 @@
+package probandoprobando7;
+
+public class trtytyr {
+private String na;
+
+public trtytyr() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+
+}
