c92299d18d5365a2477ed96f8ed6c117877e1143
Prohibidou <veramaxy@hotmail.com>
2022-08-07 14:20:45 -0300

    ok

--- ejercicio11/src/ejercicio11/Operaciones.java
+++ ejercicio11/src/ejercicio11/Operaciones.java
@@ -1,5 +1,53 @@
 package ejercicio11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Operaciones {
+
+	public ArrayList<Integer> LeoNumeros () {
+		int ingresado;
+		Scanner in = new Scanner(System.in);
+		int cont=1;
+		ArrayList<Integer> listaEleg = new ArrayList<Integer>(); //Es una lista que almacena elementos de la clase Integer.
+		do {
+			System.out.println("ingrese un entero no repetido");
+			ingresado = in.nextInt();
+			if (!listaEleg.contains(ingresado)) {
+			listaEleg.add(ingresado);
+			cont++;
+			} else System.out.println("ese elemento está en la lista, inserte otro hasta completar los 6");
+		      //notese que cuando no se añade un elemento no se aumenta el contador; solo se aumenta cuando efectivamente el valor ingresado no pertenece a la lista.
+		} while (cont<=6) ;
+		Collections.sort(listaEleg);
+		return listaEleg;
+		
+	}
+	//retorno atributos consistidos y ordenado de menor a mayor.
+	
+public ArrayList<Integer> CombGanadora () {
+	ArrayList<Integer> ListaGanadora = new ArrayList();
+	Random ran = new Random();
+    
+	for (int i=0; i<6 ; i++) {
+	ListaGanadora.add(ran.nextInt(0,50)+1); // si vale 0, valdrà finalmente 1. Si vale 48 valdrà finalmente 50. De esta manera hacemos que el metodo me devuelva entre 1 y 50 inclusive!.
+    }
+	return ListaGanadora;
+}
+
+public int comparo(ArrayList<Integer> listaEleg, ArrayList<Integer> listaGana) {
+	int contadorGanadas =0;
+	int contador =0;
+	Collections.sort(listaGana); //ordeno la lista ascendentemente, ganadora, para poder hacer busqueda binaria, y que no haya elementos repetidos
+   do {	
+	   if (listaGana.contains(listaEleg.get(contador)) ) {
+          contadorGanadas++;
+	   }
+	   contador++;
+	} while (contador<=5);
+	return contadorGanadas;
+}
 
 }
--- ejercicio11/src/ejercicio11/loteria.java
+++ ejercicio11/src/ejercicio11/loteria.java
@@ -1,10 +1,28 @@
 package ejercicio11;
 
+import java.util.ArrayList;
+
 public class loteria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+Operaciones Op1 = new Operaciones();     
+ArrayList<Integer>num = new ArrayList();
+num= Op1.LeoNumeros();
+System.out.println("Su lista de numeros elegida es:");
+for (int numLocales : num) {
+        	  System.out.println(numLocales);
+          }
+          
+ArrayList<Integer> listaGanadora = new ArrayList<Integer>() ;
+listaGanadora = Op1.CombGanadora();
+ 
+System.out.println("los numeros ganadores son;");
+for(int numeros : listaGanadora) {
+	      System.out.println(numeros);
+}
+	
+System.out.println("los numeros que acerto son: "+ Op1.comparo(num, Op1.CombGanadora()));
 	}
-
+	
 }
