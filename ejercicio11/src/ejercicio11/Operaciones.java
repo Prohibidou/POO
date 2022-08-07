0959c8d98445c6154ec2b7f420aadee64029a878
Prohibidou <veramaxy@hotmail.com>
2022-08-07 15:40:52 -0300

    11 listo

--- ejercicio11/src/ejercicio11/Operaciones.java
+++ ejercicio11/src/ejercicio11/Operaciones.java
@@ -28,13 +28,18 @@ public class Operaciones {
 	//retorno atributos consistidos y ordenado de menor a mayor.
 	
 public ArrayList<Integer> CombGanadora () {
-	ArrayList<Integer> ListaGanadora = new ArrayList();
+	ArrayList<Integer> listaGanadora = new ArrayList();
 	Random ran = new Random();
-    
-	for (int i=0; i<6 ; i++) {
-	ListaGanadora.add(ran.nextInt(0,50)+1); // si vale 0, valdrà finalmente 1. Si vale 48 valdrà finalmente 50. De esta manera hacemos que el metodo me devuelva entre 1 y 50 inclusive!.
-    }
-	return ListaGanadora;
+	int numRandom; int cont=0;
+	do { 
+		numRandom = ran.nextInt(0, 50)+1; 
+		if (!listaGanadora.contains(numRandom)){
+		listaGanadora.add(numRandom); // si vale 0, valdrà finalmente 1. Si vale 48 valdrà finalmente 50. De esta manera hacemos que el metodo me devuelva entre 1 y 50 inclusive!.
+	    cont++;
+	}
+	}while( cont<=5);
+	
+	return listaGanadora;
 }
 
 public int comparo(ArrayList<Integer> listaEleg, ArrayList<Integer> listaGana) {
@@ -42,12 +47,28 @@ public int comparo(ArrayList<Integer> listaEleg, ArrayList<Integer> listaGana) {
 	int contador =0;
 	Collections.sort(listaGana); //ordeno la lista ascendentemente, ganadora, para poder hacer busqueda binaria, y que no haya elementos repetidos
    do {	
-	   if (listaGana.contains(listaEleg.get(contador)) ) {
+	   if (listaGana.contains(listaEleg.get(contador))) {
           contadorGanadas++;
 	   }
+	   
 	   contador++;
 	} while (contador<=5);
 	return contadorGanadas;
 }
 
-}
+    public int continuar() {
+	Scanner in = new Scanner(System.in);
+	int eleccion=3; //inicializo con un numero cualquiera
+	while (eleccion>1 || eleccion<0 ) {
+		System.out.println("si desea jugar denuevo presione 1. Sino, presione 0");		
+		
+		while (!in.hasNextInt()) {
+			in.next();
+			System.out.println("ingrese un entero");
+		} eleccion=in.nextInt();
+    			
+	}
+	System.out.println("ha realizado un ingreso de datos correcto");
+	return eleccion;
+
+    }}
--- ejercicio11/src/ejercicio11/loteria.java
+++ ejercicio11/src/ejercicio11/loteria.java
@@ -7,22 +7,28 @@ public class loteria {
 	public static void main(String[] args) {
 
 Operaciones Op1 = new Operaciones();     
-ArrayList<Integer>num = new ArrayList();
-num= Op1.LeoNumeros();
+ArrayList<Integer> listaElegida = new ArrayList<Integer>();
+
+listaElegida= Op1.LeoNumeros();
 System.out.println("Su lista de numeros elegida es:");
-for (int numLocales : num) {
+//muestro los elementos de la lista leída, del usuario
+for (int numLocales : listaElegida ) {
         	  System.out.println(numLocales);
           }
           
 ArrayList<Integer> listaGanadora = new ArrayList<Integer>() ;
 listaGanadora = Op1.CombGanadora();
- 
+
+ //muestro los numeros aleatoriamente generados
 System.out.println("los numeros ganadores son;");
 for(int numeros : listaGanadora) {
 	      System.out.println(numeros);
 }
 	
-System.out.println("los numeros que acerto son: "+ Op1.comparo(num, Op1.CombGanadora()));
+System.out.println("los numeros que acerto son: "+ Op1.comparo(listaElegida , listaGanadora));
+
+int eleccionContinuar = Op1.continuar();
+
 	}
 	
 }
